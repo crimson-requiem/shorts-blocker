5554b34a53eeffdd9b77e45bd29b73441c352203
crimson-requiem <yarlagaddaritvik@gmail.com>
2025-02-15 14:03:32 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkUrl() {
+  let currentUrl = window.location.href;
+  const substring = "https://youtube.com/shorts";
+  if(string.indexOf(substring) !== -1):
+    window.location.href = "chrome://dino/"
+}
+checkUrl();
